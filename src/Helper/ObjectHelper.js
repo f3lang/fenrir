5e25d69efe1d62ae4d96b8f09698fcd72349af41
Wolfgang Felbermeier <felbermeier@bibchip.de>
2017-03-24 18:37:23 +0100

    [T] Implement fast Object property access

--- src/Helper/ObjectHelper.js
+++ src/Helper/ObjectHelper.js
@@ -1,7 +1,17 @@
+const requireFromString = require('require-from-string');
+/**
+ * Static methods to work with Objects.
+ * @author Wolfgang Felbermeier <wf@felbermeier.com>
+ */
 class ObjectHelper {
 
 	static getValueByPath(object, path){
 		let pathArray = path.split('.');
+	}
+
+	static getCompiledObjectAccessor(pathDefinition) {
+		let moduleString = "module.exports = (obj) => obj['" + pathDefinition.split('.').join("']['") + "']";
+		return requireFromString(moduleString);
 	}
 
 }
