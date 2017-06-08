7d854d6b393ac1a50c7e418ee4f26a0be264dd7e
Wolfgang Felbermeier <felbermeier@bibchip.de>
2017-06-08 10:15:32 +0200

    [T] Add integration test

---
+++ test/integration.test.js
@@ -0,0 +1,23 @@
+/**
+ * Tests the complete integration of all components
+ * @author Wolfgang Felbermeier <wf@felbermeier.com>
+ */
+
+const expect = require('chai').expect;
+const Fenrir = require('../src/Fenrir');
+
+describe("Fenrir", function() {
+
+	describe("Fenrir", function() {
+		it("creates instances of", function() {
+			let fenrir = new Fenrir(__dirname + "/integration/");
+		});
+	});
+
+	describe("Collections", function(){
+		it("creates", function(){
+
+		});
+	})
+
+});
